22811610ecf9cfcedd82819793818e85c5868e33
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2025-01-15 15:53:37 -0700

    create new habits-routes.js file and habitsController.js file

---
+++ controller/habitsController.js
@@ -0,0 +1,26 @@
+import initKnex from "knex";
+import config from "../knexfile.js";
+const knex = initKnex(config);
+
+// const getHabits = async(req, res) => {
+//     try {
+//         const data = await knex("users");
+//         res.status(200).json(data);
+//     } catch (error) {
+//         res.status(400).send(`Error retrieving Users: ${error}`);
+//     }
+// };
+
+
+// const newUser = async(req, res) => {
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// }
+
+export {
+    getUsers,
+    getUser
+}
---
+++ routes/habits-routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import * as habitsController from "../controller/usersController.js";
+
+const router = express.Router()
+// getting all the inventories
+
+export default router;
